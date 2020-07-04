b9ea0ae33a74af68d259e786cdc82cb1fff3b778
mainina9 <49619429+mainina9@users.noreply.github.com>
2020-07-04 01:07:36 -0700

    Add files via upload

---
+++ Crud.php
@@ -0,0 +1,33 @@
+<?php 
+
+   interface Crud{
+
+    
+
+    public function save();
+
+    public function readAll();
+
+    public function readUnique();
+
+    public function search();
+
+    public function update();
+
+    public function removeOne();
+
+    public function removeAll();
+
+//Lab 2 additions
+    public function validateForm();
+
+
+
+    public function createFormErrorSessions();
+ 
+
+   } 
+
+
+
+?>
